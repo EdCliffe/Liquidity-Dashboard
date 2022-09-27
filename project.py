a31dc94a5b653daaae1f887745b95e5bb8baaa6a
Ed Cliffe <ed.cliffe1@gmail.com>
2022-09-27 16:29:25 +0100

    initial commit

---
+++ project.py
@@ -0,0 +1,31 @@
+# Define targets
+
+# https://info.uniswap.org/#/pools
+# for each network
+# gather each of the 5 pages of the top pools
+
+# https://app.1inch.io/#/1/earn/pools
+# for each network, click expand for all visible entries, 
+# gather info
+
+# https://curve.fi/pools
+# After all the data is loaded,
+# scrape metrics
+
+# https://pancakeswap.finance/info
+# click through the pools to get each page
+
+# https://app.balancer.fi/#/
+# click extend and gather metrics
+
+
+
+# Scrape data from targets
+
+# Set up SQL database
+
+# Add data to database
+
+# Calculate metrics
+
+# Scrape metrics from database into dashboard
